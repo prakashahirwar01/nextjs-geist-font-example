09473ab4f35b30f5cf9016af774133e50a3fe177
prakashahirwar01 <123158821+prakashahirwar01@users.noreply.github.com>
2025-07-19 14:10:11 +0530

    Add .next/static/chunks/src_app_driver_dashboard_page_tsx_a993527f._.js 1752914410577

---
+++ .next/static/chunks/src_app_driver_dashboard_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_03e06037._.js",
+  "static/chunks/src_74f6e60d._.js"
+],
+    source: "dynamic"
+});
