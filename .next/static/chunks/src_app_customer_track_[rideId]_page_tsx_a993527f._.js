9aeaae91059c9dacb33adb73cfaf6eafbaee4899
prakashahirwar01 <123158821+prakashahirwar01@users.noreply.github.com>
2025-07-19 14:10:23 +0530

    Add .next/static/chunks/src_app_customer_track_[rideId]_page_tsx_a993527f._.js 1752914422804

---
+++ .next/static/chunks/src_app_customer_track_[rideId]_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_407583a2._.js",
+  "static/chunks/src_c641624d._.js"
+],
+    source: "dynamic"
+});
